66bbc96bbc76bd341fa87f12e66e2fc87d4f51ac
César Garcia <cesar@descom.es>
2017-09-20 17:57:25 +0200

    versioning guzzle

--- src/Http/Http.php
+++ src/Http/Http.php
@@ -2,8 +2,7 @@
 
 namespace Descom\Sms\Http;
 
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\ClientInterface;
 
 class Http
 {
@@ -21,31 +20,14 @@ class Http
      */
     public function sendHttp($verb, $path, array $headers, array $data = [])
     {
-        $client = new \GuzzleHttp\Client();
+        $version = (string) ClientInterface::VERSION;
 
-        $response = new Response();
-
-        $httpData = [
-            'headers'   => $headers,
-            'debug'     => $this->debug,
-        ];
-
-        if (count($data) > 0) {
-            $httpData['json'] = $data;
+        if ($version[0] === '6') {
+            $sms = new GuzzleV6();
+            return $sms->sendHttp($verb, $path, $headers, $data);
+        } else if ($version[0] === '5') {
+            $sms = new GuzzleV5();
+            return $sms->sendHttp($verb, $path, $headers, $data);
         }
-
-        try {
-            $result = $client->request($verb, 'https://api.descomsms.com/api/'.$path, $httpData);
-            $response->status = $result->getStatusCode();
-            $response->message = $result->getBody()->getContents();
-        } catch (ClientException $e) {
-            $response->status = $e->getResponse()->getStatusCode();
-            $response->message = $e->getResponse()->getBody(true);
-        } catch (RequestException $e) {
-            $response->status = 500;
-            $response->message = $e->getMessage();
-        }
-
-        return $response;
     }
 }
